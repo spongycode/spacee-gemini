0bb4f7cefa47e6669054c3eb04986f8e3c50ad2c
VasanthAdabala <vasanthadabala@gmail.com>
2023-12-20 20:06:38 +0530

    UI-Update

--- app/src/main/java/com/spongycode/spaceegemini/data/MainViewModel.kt
+++ app/src/main/java/com/spongycode/spaceegemini/data/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.spongycode.spaceegemini
+package com.spongycode.spaceegemini.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.GenerateContentResponse
+import com.spongycode.spaceegemini.BuildConfig
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
---
+++ app/src/main/java/com/spongycode/spaceegemini/navigation/MyNavigation.kt
@@ -0,0 +1,31 @@
+package com.spongycode.spaceegemini.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.spongycode.spaceegemini.data.MainViewModel
+import com.spongycode.spaceegemini.screens.AboutScreen
+import com.spongycode.spaceegemini.screens.HomeScreen
+
+@ExperimentalComposeUiApi
+@ExperimentalMaterial3Api
+@Composable
+fun MyNavigation(
+                 viewModel: MainViewModel,
+                 onSearchClicked: (String) -> Unit) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Home.route)
+    {
+        composable(Home.route)
+        {
+            HomeScreen(viewModel, onSearchClicked, navController)
+        }
+        composable(About.route){
+            AboutScreen(navController)
+        }
+    }
+}
